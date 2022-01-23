b946a91ccc59acde0a24ab59556668a2d3877bdb
tpmacc <tpmacc@yahoo.com>
2022-01-23 11:18:26 -0500

    add support for deployment to okteto; add actuator endpoints;

--- src/main/java/tpmacc/starter/api/GreetingController.java
+++ src/main/java/tpmacc/starter/api/GreetingController.java
@@ -2,12 +2,14 @@ package tpmacc.starter.api;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-@RestController("/v1/starter")
+@RestController
+@RequestMapping("/v1/starter")
 public class GreetingController {
 
     @Value("${starter.variable}")
