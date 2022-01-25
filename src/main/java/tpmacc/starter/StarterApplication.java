b02a0c946d741b7d463f57519780bccd1d5cb55a
tpmacc <tpmacc@yahoo.com>
2022-01-25 01:00:26 -0500

    add support for dekorate and gib; install kubernetes manifest as artifact in maven repo.

--- src/main/java/tpmacc/starter/StarterApplication.java
+++ src/main/java/tpmacc/starter/StarterApplication.java
@@ -1,9 +1,11 @@
 package tpmacc.starter;
 
+import io.dekorate.kubernetes.annotation.KubernetesApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@KubernetesApplication
 public class StarterApplication {
 
 	public static void main(String[] args) {
